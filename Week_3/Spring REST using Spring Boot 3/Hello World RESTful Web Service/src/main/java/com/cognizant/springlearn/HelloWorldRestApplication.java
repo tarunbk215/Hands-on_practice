02b732ac90fc5679febaa91618376071222eadd8
Tarunbk21 <tarunbkokatanur8762075108@gmail.com>
2026-07-26 13:17:36 +0530

    RESTFull

--- Week_3/Spring REST using Spring Boot 3/Hello World RESTful Web Service/src/main/java/com/cognizant/springlearn/HelloWorldRestApplication.java
+++ Week_3/Spring REST using Spring Boot 3/Hello World RESTful Web Service/src/main/java/com/cognizant/springlearn/HelloWorldRestApplication.java
@@ -22,13 +22,13 @@ public class HelloWorldRestApplication {
 		logger.info("=================================================================");
 		logger.info("Starting Hello World RESTful Web Service Application...");
 		logger.info("=================================================================");
-		
+
 		// Demonstrate Spring XML Configuration with SimpleDateFormat
 		displayDate();
-		
+
 		// Start the Spring Boot application
 		SpringApplication.run(HelloWorldRestApplication.class, args);
-		
+
 		logger.info("=================================================================");
 		logger.info("Hello World REST Application started successfully!");
 		logger.info("Application is running on: http://localhost:8083");
@@ -45,13 +45,13 @@ public class HelloWorldRestApplication {
 			SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 			String dateString = "31/12/2018";
 			Date parsedDate = format.parse(dateString);
-			
+
 			System.out.println("Original date string: " + dateString);
 			System.out.println("Parsed Date object: " + parsedDate);
 			System.out.println("Formatted back to string: " + format.format(parsedDate));
-			
+
 			logger.info("Successfully loaded SimpleDateFormat from Spring XML Configuration");
-			
+
 		} catch (Exception e) {
 			logger.error("Error in displayDate() method: " + e.getMessage());
 		}
